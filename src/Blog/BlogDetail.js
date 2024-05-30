99828ed8a4047f317082931d55dfecfd04f909e4
Tiến Phan <bonpro2701@gmail.com>
2024-05-31 01:18:54 +0700

    3/4

--- src/Blog/BlogDetail.js
+++ src/Blog/BlogDetail.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Rate from "../Comment/Rate";
-import Comment from "../Comment/Comment"
-import Replay from "../Comment/Replay"
+import Comment from "../Comment/Comment";
+import Replay from "../Comment/Replay";
 function BlogDetail(props) {
   let params = useParams();
   const [data, setData] = useState([]);
@@ -12,7 +12,7 @@ function BlogDetail(props) {
       .get("http://localhost/laravel8/public/api/blog/detail/" + params.id)
       .then((res) => {
         setData(res.data.data);
-        console.log(res.data.data);
+        // console.log(res.data.data);
       });
   }, []);
   function renderData() {
@@ -68,55 +68,12 @@ function BlogDetail(props) {
             <img src="/images/blog/socials.png" alt="" />
           </a>
         </div>
-
-
-        {/* <div class="media commnets">
-          <a class="pull-left" href="#">
-            <img class="media-object" src="/images/blog/man-one.jpg" alt="" />
-          </a>
-          <div class="media-body">
-            <h4 class="media-heading">Annie Davis</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <div class="blog-socials">
-              <ul>
-                <li>
-                  <a href="">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <i class="fa fa-dribbble"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="">
-                    <i class="fa fa-google-plus"></i>
-                  </a>
-                </li>
-              </ul>
-              <a class="btn btn-primary" href="">
-                Other Posts
-              </a>
-            </div>
-          </div>
-        </div> */}
         {/* start comment */}
         <Comment></Comment>
         {/* end comment */}
 
         {/* start replay */}
-        <Replay></Replay>
+        <Replay idBlog={params.id}></Replay>
         {/* end replay */}
       </div>
     </>
--- src/Comment/Replay.js
+++ src/Comment/Replay.js
@@ -1,25 +1,136 @@
-function Replay(){
-    return(
-        <>
-        <div class="replay-box">
-          <div class="row">
-            <div class="col-sm-12">
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+function Replay(props) {
+  let params = useParams();
+  const [isLogin, setIsLogin] = useState(false);
+  const [errors, setErrors] = useState("");
+  const [input, setInput] = useState({
+    comment: "",
+  });
+  const [checkLogin, setCheckLogin] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedLogin = localStorage.getItem("checkLogin");
+    if (storedLogin) {
+      const parsedLogin = JSON.parse(storedLogin);
+      console.log(parsedLogin);
+      setIsLogin(true);
+      setCheckLogin(parsedLogin);
+      setAccessToken(parsedLogin.token); // Lấy token từ dữ liệu trong localStorage
+    }
+  }, []);
+
+  const handelPostComment = () => {
+    if (isLogin) {
+      console.log("Đã login");
+    } else {
+      console.log("Chưa login");
+      navigate("/login");
+    }
+  };
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInput((state) => ({ ...state, [name]: value }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let errorSubmit = {};
+    let flag = true;
+
+    if (input.comment === "") {
+      errorSubmit.comment = "hãy nhập comment";
+      console.log("hãy nhập comment");
+      flag = false;
+    }
+
+    if (!flag) {
+      setErrors(errorSubmit);
+    } else {
+      setErrors({});
+      let url =
+        "https://localhost/laravel8/public/api/blog/comment/" + params.id;
+      let config = {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "application/json",
+        },
+      };
+
+      if (checkLogin && checkLogin.Auth) {
+        const formData = new FormData();
+        formData.append("id_blog", props.idBlog);
+        formData.append("id_user", checkLogin.Auth.id);
+        formData.append("name_user", checkLogin.Auth.name);
+        formData.append("id_comment", 0);
+        formData.append("comment", input.comment);
+        formData.append("image_user", checkLogin.Auth.avatar);
+
+        // Log dữ liệu để kiểm tra
+        console.log("Form Data:");
+        console.log("id_blog:", props.idBlog);
+        console.log("id_user:", checkLogin.Auth.id);
+        console.log("name_user:", checkLogin.Auth.name);
+        console.log("id_comment:", 0);
+        console.log("comment:", input.comment);
+        console.log("image_user:", checkLogin.Auth.avatar);
+
+        axios
+          .post(url, formData, config)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((error) => {
+            console.error("Error posting comment:", error);
+          });
+      } else {
+        console.error("User is not logged in. Redirecting to login page.");
+        navigate("/login");
+      }
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="replay-box">
+          <div className="row">
+            <div className="col-sm-12">
               <h2>Leave a replay</h2>
 
-              <div class="text-area">
-                <div class="blank-arrow">
+              <div className="text-area">
+                <div className="blank-arrow">
                   <label>Your Name</label>
                 </div>
                 <span>*</span>
-                <textarea name="message" rows="11"></textarea>
-                <a class="btn btn-primary" href="">
+                <textarea
+                  value={input.comment}
+                  onChange={handleChange}
+                  name="comment"
+                  rows="11"
+                ></textarea>
+                <button
+                  type="submit"
+                  onClick={handelPostComment}
+                  className="btn btn-primary"
+                  href=""
+                >
                   post comment
-                </a>
+                </button>
               </div>
             </div>
           </div>
         </div>
-        </>
-    )
+      </form>
+    </>
+  );
 }
+
 export default Replay;
