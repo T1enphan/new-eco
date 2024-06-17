a5aa396e07c6b288f7b548aaf645ae105579d7c9
Tiến Phan <bonpro2701@gmail.com>
2024-06-17 14:03:35 +0700

    delete product

--- src/Product/getProduct.js
+++ src/Product/getProduct.js
@@ -30,6 +30,64 @@ function GetMyProduct() {
 
     fetchData();
   }, []);
+
+  // const handleDelete = async (id) => {
+  //   if (accessToken) {
+  //     const headers = {
+  //       Authorization: `Bearer ${accessToken}`,
+  //     };
+
+  //     try {
+  //       const res = await axios.post(
+  //         `http://localhost/laravel8/public/api/user/product/delete/${id}`,
+  //         { headers }
+  //       );
+  //       if (res.status === 200) {
+  //         // Xóa sản phẩm thành công, cập nhật lại dữ liệu
+  //         setData((prevData) => {
+  //           const updatedData = { ...prevData };
+  //           delete updatedData[id];
+  //           return updatedData;
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // };
+
+  const handleDelete = async (id) => {
+    if (accessToken) {
+      const headers = {
+        Authorization: `Bearer ${accessToken}`,
+      };
+
+      try {
+        const res = await axios.post(
+          `http://localhost/laravel8/public/api/user/product/delete/${id}`,
+          {},
+          { headers }
+        );
+        if (res.status === 200) {
+          // Xóa sản phẩm thành công, cập nhật lại dữ liệu
+          setData((prevData) => {
+            const updatedData = { ...prevData };
+            delete updatedData[id];
+            return updatedData;
+          });
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          console.log("Unauthorized: Invalid token");
+        } else {
+          console.log(error);
+        }
+      }
+    } else {
+      console.log("No access token found");
+    }
+  };
+
   function renderData() {
     console.log(data);
     if (data) {
@@ -40,12 +98,17 @@ function GetMyProduct() {
             <th scope="row">{index + 1}</th>
             <td>{item.name}</td>
             <td>
-              <img src={item.image} width="50" height="50" />
+              <img src={item.image} width="50" height="50" alt={item.name} />
             </td>
             <td>{item.price}</td>
             <td className="text-center">
               <button className="btn btn-warning">Edit</button>
-              <button className="btn btn-danger">Delete</button>
+              <button
+                className="btn btn-danger"
+                onClick={() => handleDelete(item.id)}
+              >
+                Delete
+              </button>
             </td>
           </tr>
         );
@@ -54,6 +117,7 @@ function GetMyProduct() {
       console.log(2);
     }
   }
+
   return (
     <>
       <div className="col-sm-9">
